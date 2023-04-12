6d5d30fde70d95bce34340d8fd766023a2debea2
HP <aliimam@hacettepe.edu.tr>
2023-04-12 17:54:44 +0300

    added

--- src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -1,6 +1,6 @@
-package com.luv2code.springcoredemo;
+package com.luv2code.springcoredemo.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.luv2code.springcoredemo.common.Coach;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +11,13 @@ public class DemoController {
     private Coach myCoach;
 
     //define a constructor for dependency injection
-    @Autowired
+
     public DemoController(Coach theCoach) {
         myCoach = theCoach;
     }
 
     @GetMapping("/dailyworkout")
-    public String getDailyWorkout(){
+    public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
     }
 }
